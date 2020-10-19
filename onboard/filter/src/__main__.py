fe282fab2b6ca59f2084286b0b1737fb6bbb8fa0
rzf16 <ryan.feng16@gmail.com>
2020-10-19 14:56:16 -0400

    Add FilterType option for sensor piping. Add turn states to static nav states.

--- onboard/filter/src/__main__.py
+++ onboard/filter/src/__main__.py
@@ -123,7 +123,9 @@ class SensorFusion:
         self.gps = Gps()
         self.imu = Imu(self.config["IMU_accel_filter_bias"], self.config["IMU_accel_threshold"])
         self.nav_state = None
-        self.static_nav_states = {None, "Off", "Done", "Search Spin Wait", "Turned to Target Wait", "Gate Spin Wait"}
+        self.static_nav_states = {None, "Off", "Done", "Search Spin Wait", "Turned to Target Wait", "Gate Spin Wait",
+                                  "Turn", "Search Turn", "Turn to Target", "Turn Around Obstacle", "Search Turn Around Obstacle",
+                                  "Gate Turn", "Gate Turn to Center Point", "Radio Repeater Turn"}
 
         self.filter = None
         self.state_estimate = StateEstimate(ref_lat=self.config["RefCoords"]["lat"],
@@ -142,25 +144,10 @@ class SensorFusion:
         if self.filter is None and self.gps.ready():
             self._constructFilter()
             self.gps.fresh = False
-        # if self.gps.ready() and self.imu.ready():
-        #     odom = Odometry()
-        #     pos = self.gps.pos.asMinutes()
-        #     odom.latitude_deg = pos["lat_deg"]
-        #     odom.latitude_min = pos["lat_min"]
-        #     odom.longitude_deg = pos["long_deg"]
-        #     odom.longitude_min = pos["long_min"]
-        #     odom.bearing_deg = self.imu.bearing.bearing_deg
-        #     vel = self.gps.vel.absolutify(0)
-        #     odom.speed = np.hypot(vel["north"], vel["east"])
-        #     self.lcm.publish('/odometry', odom.encode())
-
 
     def _imuCallback(self, channel, msg):
         new_imu = IMUData.decode(msg)
         self.imu.update(new_imu)
-        # DEBUG
-        # with open("odom_accel_log.json", 'a') as out:
-        #     out.write('{"accel_x_g": ' + str(self.imu.accel.accel_x / 9.8) + '},\n')
 
     def _navStatusCallback(self, channel, msg):
         new_nav_status = NavStatus.decode(msg)
@@ -169,6 +156,18 @@ class SensorFusion:
     def _constructFilter(self):
         '''
         Constructs filter depending on filter type
+        '''
+
+        if self.config["FilterType"] == "LinearKalman":
+            self._constructLKF()
+        elif self.config["FilterType"] == "Pipe":
+            self.filter = "Pipe"
+        else:
+            raise ValueError("Invalid filter type!")
+    
+    def _constructLKF(self):
+        '''
+        Constructs a Linear Kalman Filter
         '''
         ref_bearing = self._getFreshBearing()
         if ref_bearing is None:
@@ -185,28 +184,91 @@ class SensorFusion:
                                             ref_lat=self.config["RefCoords"]["lat"],
                                             ref_long=self.config["RefCoords"]["long"])
 
-        if self.config['FilterType'] == 'LinearKalman':
-            P_initial = self.config['P_initial']
-            R = self.config['R_Dynamic']
+        P_initial = self.config['P_initial']
+        R = self.config['R_Dynamic']
 
-            F = np.array([[1., dt, 0., 0.],
-                          [0., 1., 0., 0.],
-                          [0., 0., 1., dt],
-                          [0., 0., 0., 1.]])
+        F = np.array([[1., dt, 0., 0.],
+                     [0., 1., 0., 0.],
+                     [0., 0., 1., dt],
+                     [0., 0., 0., 1.]])
 
-            B = np.array([[0.5*dt**2., 0.],
-                         [dt, 0.],
-                         [0., 0.5*dt**2.],
-                         [0., dt]])
+        B = np.array([[0.5*dt**2., 0.],
+                     [dt, 0.],
+                     [0., 0.5*dt**2.],
+                     [0., dt]])
 
-            H = np.eye(4)
+        H = np.eye(4)
 
-            Q = QDiscreteWhiteNoise(2, dt, self.config["Q_Dynamic"], 2)
+        Q = QDiscreteWhiteNoise(2, dt, self.config["Q_Dynamic"], 2)
 
-            self.filter = LinearKalmanFilter(4, 4, dim_u=2)
-            self.filter.construct(self.state_estimate, P_initial, F, H, Q, R, B=B)
+        self.filter = LinearKalmanFilter(4, 4, dim_u=2)
+        self.filter.construct(self.state_estimate, P_initial, F, H, Q, R, B=B)
+    
+    def _runLKF(self):
+        '''
+        Runs an iteration of a Linear Kalman Filter
+        '''
+        bearing = self._getFreshBearing()
+        pos = self._getFreshPos()
+        # vel = self._getFreshVel(bearing)
+        vel = None
+        accel = self._getFreshAccel(bearing)
+
+        u = np.array([accel["north"], accel["east"]]) if accel is not None else None
+        Q = None
+        if self.nav_state in self.static_nav_states:
+            Q = QDiscreteWhiteNoise(2, self.config["dt"], self.config["Q_Static"], 2)
+        self.filter.predict(u=u, Q=Q)
+
+        # Zero velocity if in a static nav state
+        if self.nav_state in self.static_nav_states:
+            self._zeroVel()
+
+        z = None
+        H = None
+        # vel = {"north": np.asscalar(self.filter.x[1]), "east": np.asscalar(self.filter.x[3])}
+        if pos is not None:
+            pos_meters = {}
+            pos_meters["long"] = long2meters(pos["long"], pos["lat"],
+                                                ref_long=self.config["RefCoords"]["long"])
+            pos_meters["lat"] = lat2meters(pos["lat"],
+                                            ref_lat=self.config["RefCoords"]["lat"])
+            if vel is not None:
+                # If both position and velocity are available, use both
+                z = np.array([pos_meters["lat"], vel["north"], pos_meters["long"],
+                                vel["east"]])
+                H = np.eye(4)
+            else:
+                # If only position is available, zero out the velocity residual
+                vel = self.state_estimate.vel
+                z = np.array([pos_meters["lat"], vel["north"], pos_meters["long"], vel["east"]])
+                # z = np.array([pos_meters["lat"], 0, pos_meters["long"], 0])
+                # H = np.diag([1, 0, 1, 0])
+                H = np.eye(4)
         else:
-            raise ValueError("Invalid filter type!")
+            if vel is not None:
+                # If only velocity is availble, zero out the position residual
+                z = np.array([0, vel["north"], 0, vel["east"]])
+                H = np.diag([0, 1, 0, 1])
+
+        # Full faith in velocity measurement if static
+        R = None
+        if self.nav_state in self.static_nav_states:
+            R = self.config["R_Static"]
+        self.filter.update(z, H=H, R=R)
+
+        # Zero velocity if in a static nav state
+        if self.nav_state in self.static_nav_states:
+            self._zeroVel()
+
+        print(self.filter.x)
+
+        self.state_estimate.updateFromLKF(self.filter.x)
+        if bearing is not None:
+            self.state_estimate.bearing_deg = bearing
+
+        self.gps.fresh = False
+        self.imu.fresh = False
 
     def _getFreshBearing(self):
         '''
@@ -221,14 +283,17 @@ class SensorFusion:
         else:
             return None
 
-    def _getFreshPos(self):
+    def _getFreshPos(self, decimal=True):
         '''
         Returns a fresh GPS position to use. Returns None if no fresh sensors
 
         @return dict/None: GPS coordinates (decimal degrees)
         '''
         if time.time() - self.gps.last_fresh <= self.config["GPS_fresh_timeout"]:
-            return self.gps.pos.asDecimal()
+            if decimal:
+                return self.gps.pos.asDecimal()
+            else:
+                return self.gps.pos.asMinutes()
         else:
             return None
 
@@ -277,68 +342,20 @@ class SensorFusion:
         '''
         while True:
             if self.filter is not None:
-                bearing = self._getFreshBearing()
-                pos = self._getFreshPos()
-                # vel = self._getFreshVel(bearing)
-                vel = None
-                accel = self._getFreshAccel(bearing)
+                if self.config["FilterType"] == "LinearKalman":
+                    self._runLKF()
+                elif self.config["FilterType"] == "Pipe":
+                    bearing = self._getFreshBearing()
+                    if bearing is None:
+                        continue
+                    pos = self._getFreshPos(decimal=False)
+                    vel = self._getFreshVel(bearing)
 
-                u = np.array([accel["north"], accel["east"]]) if accel is not None else None
-                Q = None
-                if self.nav_state in self.static_nav_states:
-                    Q = QDiscreteWhiteNoise(2, self.config["dt"], self.config["Q_Static"], 2)
-                self.filter.predict(u=u, Q=Q)
-
-                # Zero velocity if in a static nav state
-                if self.nav_state in self.static_nav_states:
-                    self._zeroVel()
-
-                z = None
-                H = None
-                # vel = {"north": np.asscalar(self.filter.x[1]), "east": np.asscalar(self.filter.x[3])}
-                if pos is not None:
-                    pos_meters = {}
-                    pos_meters["long"] = long2meters(pos["long"], pos["lat"],
-                                                     ref_long=self.config["RefCoords"]["long"])
-                    pos_meters["lat"] = lat2meters(pos["lat"],
-                                                   ref_lat=self.config["RefCoords"]["lat"])
-                    if vel is not None:
-                        # If both position and velocity are available, use both
-                        z = np.array([pos_meters["lat"], vel["north"], pos_meters["long"],
-                                      vel["east"]])
-                        H = np.eye(4)
-                    else:
-                        # If only position is available, zero out the velocity residual
-                        vel = self.state_estimate.vel
-                        z = np.array([pos_meters["lat"], vel["north"], pos_meters["long"], vel["east"]])
-                        # z = np.array([pos_meters["lat"], 0, pos_meters["long"], 0])
-                        # H = np.diag([1, 0, 1, 0])
-                        H = np.eye(4)
-                else:
-                    if vel is not None:
-                        # If only velocity is availble, zero out the position residual
-                        z = np.array([0, vel["north"], 0, vel["east"]])
-                        H = np.diag([0, 1, 0, 1])
-
-                # Full faith in velocity measurement if static
-                R = None
-                if self.nav_state in self.static_nav_states:
-                    R = self.config["R_Static"]
-                self.filter.update(z, H=H, R=R)
-
-                # Zero velocity if in a static nav state
-                if self.nav_state in self.static_nav_states:
-                    self._zeroVel()
-
-                print(self.filter.x)
-
-                self.state_estimate.updateFromLKF(self.filter.x)
-                if bearing is not None:
-                    self.state_estimate.bearing_deg = bearing
-
-                self.gps.fresh = False
-                self.imu.fresh = False
-
+                    self.state_estimate = StateEstimate(pos["lat_deg"], pos["lat_min"], vel["north"],
+                                                        pos["long_deg"], pos["long_min"], vel["east"],
+                                                        bearing,
+                                                        ref_lat=self.config["RefCoords"]["lat"],
+                                                        ref_long=self.config["RefCoords"]["long"])
                 odom = self.state_estimate.asOdom()
                 self.lcm.publish('/odometry', odom.encode())
             await asyncio.sleep(self.config["UpdateRate"])
@@ -347,7 +364,6 @@ class SensorFusion:
 def main():
     fuser = SensorFusion()
     run_coroutines(fuser.lcm.loop(), fuser.run())
-    # run_coroutines(fuser.lcm.loop())
 
 
 if __name__ == '__main__':
